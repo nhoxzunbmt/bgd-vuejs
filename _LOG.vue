f4f0c18e031d970bbb707db31091c346a9591274
nhoxzunbmt <nhoxzunbmt@users.noreply.github.com>
2018-01-30 18:49:59 +0700

    Update

---
+++ _LOG.vue
@@ -0,0 +1,32 @@
+<script>
+import articleList from "../../components/article/list";
+
+export default {
+  data() {
+    return {
+      articleList: {},
+    }
+  },
+
+  mounted() {
+
+
+
+  },
+
+  components: {
+    articleList
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+
+    reload() {
+      window.location.reload();
+    }
+  },
+}
+</script>
--- components/article/list.vue
+++ components/article/list.vue
@@ -1,12 +1,9 @@
-<template>
-<v-container grid-list-md text-xs-center>
-  <v-layout row wrap>
-
-
-
-    <v-flex xs12 sm6 md4 v-for="i in 3" :key="i">
+<v-flex xs12 sm6 offset-sm3>
       <v-card>
-        <v-card-media src="https://vuetifyjs.com/static/doc-images/cards/sunshine.jpg" height="200px">
+        <v-card-media
+          src="https://vuetifyjs.com/static/doc-images/cards/sunshine.jpg"
+          height="200px"
+        >
         </v-card-media>
         <v-card-title primary-title>
           <div>
@@ -24,14 +21,8 @@
         </v-card-actions>
         <v-slide-y-transition>
           <v-card-text v-show="show">
-            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we
-            walk! You're going to do his laundry? I've got to find a way to escape.
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
           </v-card-text>
         </v-slide-y-transition>
       </v-card>
     </v-flex>
-
-
-  </v-layout>
-</v-container>
-</template>
